12152d0cf96b9e2637b94b9624207385e8855ea1
adit1611 <aditya16112002@gmail.com>
2024-09-24 17:25:02 +0530

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import { ThemeProvider } from './Theme'; 
+const App = () => {
   return (
-    <>
-    <h1 className="text-3xl bg-sky-400 font-bold underline">
+    <ThemeProvider>
+      <h1 className="text-3xl bg-sky-400 font-bold underline">
     Hello world!
-  </h1>
-    </>
-  )
-}
+      </h1>
+      </ThemeProvider>
+    );
+  };
 
 export default App
